82f59112d011955eca5769e548df826a359a0474
cis <cis@CISM281.(none)>
2016-05-31 13:53:31 +0530

    hide incomplate task

--- client/main2.js
+++ client/main2.js
@@ -1,13 +1,29 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 import './task.js';
 import './main2.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
  Template.body.helpers({
     tasks:function () {
+            const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
         //console.log(Tasks.find().fetch());
        return Tasks.find({}, { sort: { createdAt: -1 } });
-  }
+  },
+    incompleteCount() {
+
+    return Tasks.find({ checked: { $ne: true } }).count();
+
+  },
 });
 
 Template.body.events({
@@ -39,6 +55,10 @@ Template.body.events({
     target.text.value = '';
 
   },
+   'change .hide-completed input'(event, instance) {
 
+    instance.state.set('hideCompleted', event.target.checked);
+
+  },
 });
 
